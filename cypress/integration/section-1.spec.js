60ccf87cafd5299ea7fca7325d271d43abb8a1ec
R4myc0d3s <83361019+R4myc0d3s@users.noreply.github.com>
2022-05-23 04:36:41 -0400

    Update section-1.spec.js

--- cypress/integration/section-1.spec.js
+++ cypress/integration/section-1.spec.js
@@ -1,10 +1,39 @@
 const { Section1 } = require('../objects/section-1')
 
-describe('Problem 1', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section1, you can do: Section1.actions.assertSampleApiResponse();
-   *
-   * Test away!
-   */
+describe('DOM Table and Form', () => {
+  beforeEach(function () {
+    cy.visit('/section-1.html')
+  })
+
+  it('Assert Table is not visible', () => {
+    Section1.actions.TableNotVisible()
+  })
+
+  it('Show table and assert that it is visible', () => {
+    Section1.actions.ShowTable()
+    Section1.actions.TableVisible()
+  })
+
+  it('Table is 5 clmns and 10 rows', () => {
+    Section1.actions.ShowTable()    
+    Section1.actions.TableHasFiveClmns()
+  })  
+
+  it('Assert form is not visible', () => {
+    Section1.actions.FormNotVisible
+  }) 
+  it('Show form and assert that it is visible', () => {
+    Section1.actions.ShowForm()
+    Section1.actions.FormVisble()
+  }) 
+  it('Fill Form, submit and check if submitted', () => {
+    Section1.actions.ShowForm()    
+    Section1.actions.FillFormNameAndAge()
+    Section1.actions.FormNameAgeFilled()
+    Section1.actions.FormCheckNurse()
+    Section1.actions.FormIsNurseChecked()
+    Section1.actions.SubmitAndCheckAlert()
+  }) 
 })
+
+
